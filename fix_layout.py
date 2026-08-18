3acd056e0c6705a05f2544240fa6eeaca9ec0037
Quoc Hoang Ng <168975831+hqcoder05@users.noreply.github.com>
2026-08-18 17:35:13 +0700

    Delete fix_layout.py

--- fix_layout.py
+++
@@ -1,8 +0,0 @@
-﻿import os
-with open('d:/okemsocial/Views/Shared/_Layout.cshtml', 'r', encoding='utf-8-sig', errors='ignore') as f:
-    text = f.read()
-
-text = text.replace(r'\"U\"', '"U"')
-
-with open('d:/okemsocial/Views/Shared/_Layout.cshtml', 'w', encoding='utf-8-sig') as f:
-    f.write(text)
